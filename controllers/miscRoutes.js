7042e062f77512506085b5a0d650cd3373231b5f
Ikey Benzaken <ikey.benz@gmail.com>
2018-11-07 14:06:47 -0800

    Added test folder with mocha and chai

--- controllers/miscRoutes.js
+++ controllers/miscRoutes.js
@@ -3,7 +3,7 @@ const Post = require('../models/post');
 
 router.get('/', (req, res) => {
     Post.find({}).then(posts => {
-        res.render('index', { posts: posts });
+        res.render('posts/index', { posts: posts });
     });
 });
 
